c0385a78c713640ea6fdf1cc59f895b0fb1cdbe8
sandesh-543 <2022uch1159@mnit.ac.in>
2025-08-03 14:50:49 +0530

    Added error handling on database pooling

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -9,6 +9,11 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
 });
 
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
+
 module.exports = {
   query: (text, params) => pool.query(text, params),
   pool,
